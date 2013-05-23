4a172f6b576135db7eeafb27dc4747a4c295112a
Justin Sorensen <justinsorensen83@gmail.com>
2013-05-23 00:26:36 -0700

    first commit
    storing some stuff

---
+++ README.md
@@ -0,0 +1,11 @@
+home for testing some fb login stuff with the graph api.
+need to create .env file for testing purposes
+
+"
+PORT=5000
+MONGO_HOST=localhost
+MONGO_PORT=27017
+FB_APP_ID=YOUR_APP_ID
+FB_SECRET=YOUR_APP_SECRET
+APP_VER=1
+LOGGER=1
---
+++ main.js
@@ -0,0 +1,58 @@
+
+var http = require('http'),
+    assert = require('assert'),
+    fs = require('fs'),
+    url = require('url'),
+    path = require('path');
+	//fb = require('./fb');
+
+var port = process.env.PORT || 5000;
+var webPath = 'public';
+var mimeTypes = {
+  '.js' : 'application/javascript',
+  '.html' : 'text/html',
+  '.jpeg' : 'image/jpeg',
+  '.jpg' : 'image/jpg',
+  '.gif' : 'image/gif',
+  '.css' : 'text/css',
+  '.ico' : 'image/x-icon'
+};
+
+function requestHandler(req, res) {
+  var requestedUrl = url.parse(req.url).pathname;
+  if(requestedUrl === '/') requestedUrl += 'index.html';
+  var requestedFile = path.join(webPath, requestedUrl);
+  console.log(req.connection.remoteAddress + ' requested file ' + requestedFile);
+  fs.exists(requestedFile, function(exists) {
+    if(!exists) {
+      res.writeHead(404, {
+        'Content-Type': 'text/plain'
+      });
+      res.write('404 Not Found');
+      res.end();
+      return;
+    }
+    fs.readFile(requestedFile, 'binary', function(err, file) {
+      if(err) {
+        res.writeHead(500, {
+          'Content-Type': 'text/plain'
+        });
+        res.write(err);
+        res.end();
+        return;
+      }
+      var type = mimeTypes[path.extname(requestedFile)];
+      res.writeHead(200, {
+        'Content-Type': type
+      });
+      res.write(file, 'binary');
+      res.end();
+    });
+  });
+}
+
+
+var server = http.createServer(requestHandler);
+server.listen(port, function() { 
+  console.log('Listening to the port ' + port);  
+});
